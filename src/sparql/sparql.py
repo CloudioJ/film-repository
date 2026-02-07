da055386503e5fd31b620922facdbe17ca95bed1
Claudio <claudio.junior.ts2@gmail.com>
2026-02-07 17:25:45 -0300

    [REFACT] - Ajuste nas queries

--- src/sparql/sparql.py
+++ src/sparql/sparql.py
@@ -6,30 +6,54 @@ class SparQL:
     def __init__(self, ontology_path: str):
         self.g = Graph()
         self.g.parse(ontology_path)
-
-    def query(self, actor: str = "", title: str = "", year: str = "") -> list[dict]:
-        parameters = build_parameters(actor, title, year)
-
-        prefix = (
-            "PREFIX foaf: <http://www.ime.usp.br/~renata/FOAF-modified>"
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+        self.prefix = (
+            "PREFIX foaf: <http://www.ime.usp.br/~renata/FOAF-modified>\n"
+            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
             "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n\n"
         )
 
-        sparql_query = prefix + (
+
+
+    def query_by_actor(self, actor: str = ""):
+
+        print("[INFO] Procurando por ator")
+
+        sparql_query = self.prefix + (
             "SELECT DISTINCT ?Movies\n"
             "WHERE {\n"
-            f"  {parameters}\n"
+            f'?dir rdfs:label "{actor}" .'
+            '?dir foaf:acts ?mov .'
+            '?mov rdfs:label ?Movies .'
             "}\n"
         )
 
-        print(f"Query enviado:\n{sparql_query}")
+        query_return = self.g.query(sparql_query)
 
         results = []
-        for row in self.g.query(sparql_query):
+        for row in query_return:
             print(row)
             results.append({
                 "movie_title": str(row.Movies) if getattr(row, 'Movies', None) is not None else None,
             })
 
         return results
+    
+    def query_by_movie(self, movie: str = ""):
+        
+        return
+    
+    def query_by_director(self, director: str = ""):
+        return
+    
+    def query(self, actor: str = "", title: str = "", director: str = "") -> list[dict]:
+
+        if actor:
+            return self.query_by_actor(actor)
+        
+        if title:
+            return self.query_by_actor(title)
+        
+        if director:
+            return self.query_by_actor(director)
+        
+        return []
